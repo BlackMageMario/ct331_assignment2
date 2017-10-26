53187159423ea1a717d6c32b823a55e6fa572d0c
Eamonn F. Hannon <blackmagemario@gmail.com>
2017-10-26 18:18:02 +0100

    Progress as of 18:17 26th October 2017

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -14,6 +14,9 @@
 (provide ins_beg)
 (provide ins_end)
 (provide cout_top_level)
+(provide count_instances)
+(provide count_instances_tr)
+(provide count_instances_deep)
 (define (ins_beg el lst)
   (cons el (cons lst empty)))
 
@@ -24,3 +27,43 @@
   (if(null? lst)
      0
      (+ 1 (cout_top_level(cdr lst)))))
+;no need for nested cond
+(define (count_instances lst item)
+  (cond
+    [(empty? lst) 0]
+    [(equal? item (car lst))
+        (+ 1 (count_instances (cdr lst) item))]
+    [else (count_instances (cdr lst) item)]
+  )
+ )
+
+(define (count_instances_tr_helper lst item)
+  (count_instances_tr lst item 0)
+  )
+;tail recursive: we just call the function and add 1 to our out value
+;if an item was found
+(define (count_instances_tr lst item out)
+  (cond
+    [(empty? lst) out]
+    [(equal? item (car lst))
+     (count_instances_tr (cdr lst) item (+ 1 out))]
+    [else (count_instances_tr (cdr lst) item out)]
+    )
+  )
+(define (count_instances_deep_helper lst item)
+  (count_instances_deep lst item 0)
+  )
+;similiar to the above
+;except we have a new condition: if we find that
+;the (car lst) is a list element itself, we count the instances in that
+;and add it to the count instances of (cdr lst)
+(define (count_instances_deep lst item out)
+  (cond
+    [(empty? lst) out]
+    [(equal? item (car lst))
+             (count_instances_deep (cdr lst) item (+ 1 out))]
+    [(list? (car lst))
+     (+ (count_instances_deep (car lst) item 0) (count_instances_deep (cdr lst) item out))]
+    [else ((count_instances_deep (cdr lst) item out))]
+    )
+  )
--- assignment_q3.rkt
+++ assignment_q3.rkt
@@ -1,1 +1,76 @@
 #lang racket
+(define (binary_tree_leaf value )
+  (list '() value '() )
+)
+
+(define (binary_tree_node left_node value right_node)
+  (list left_node value right_node))
+;one more lab to go
+(define (traverse tree)
+  (cond
+    [(empty? tree)]
+    [else
+     (begin
+         (traverse (car tree))
+         (printf" ~a "(cadr tree))
+         (traverse (caddr tree))
+      )
+    ]    
+  )
+)
+
+(define test_tree
+  (binary_tree_node
+   (binary_tree_node
+    (binary_tree_leaf 1)
+    3
+    (binary_tree_leaf 6)
+    )
+   8
+   (binary_tree_node
+    (binary_tree_leaf 9)
+    10
+    (binary_tree_leaf 11)
+    )
+   )
+  )
+(define (search_tree tree value)
+  (cond
+    [(empty? tree) #f]
+    [(= value (cadr tree))]
+    [else(search_tree (car tree ) value)(search_tree (caddr tree) value)]
+    )
+  )
+(define (insert_tree tree value)
+  (display tree)
+  (display "\n")
+  (cond
+    [(empty? tree)(binary_tree_leaf value)]
+    [(= (cadr tree) value) #t]
+    [(> (cadr tree) value)(list (insert_tree (car tree) value) (cadr tree) (caddr tree))]
+    [(< (cadr tree) value)(list (car tree) (cadr tree) (insert_tree (caddr tree) value) )]        
+  )
+)
+(define (insert_list_tree tree list)
+  (cond
+    [(empty? list) tree]
+    [else (insert_list_tree (insert_tree tree (car list)) (cdr list))]
+   )
+)
+;
+(define (ascending_sort tree lowest_value)
+  (cond
+    [(empty? tree) lowest_value]
+    [(< (cadr tree) lowest_value)(list (ascending_sort (car tree) (cadr tree))(ascending_sort (caddr tree) lowest_value))]
+    [(> (cadr tree) lowest_value)(list (ascending_sort (car tree) lowest_value)(ascending_sort (caddr tree) (cadr tree)))]
+  )
+)
+(define (descending_sort tree highest_value)
+  (cond
+    [(empty? tree) highest_value]
+    [(< (cadr tree) highest_value)(list (descending_sort (car tree) highest_value)(descending_sort (caddr tree) (cadr tree)))]
+    [(> (cadr tree) highest_value)(list (descending_sort (car tree) (cadr tree))(descending_sort (caddr tree) highest_value))]
+   )
+ )
+(define test_list
+  (list 14 28 7))
